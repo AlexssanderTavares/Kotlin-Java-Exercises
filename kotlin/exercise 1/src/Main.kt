989d82dc3e52ed2249dcf840fcfeb2fd73e88537
Alexssander Tavares <alexssander.tavares.dev@gmail.com>
2024-08-16 16:22:19 -0300

    adding first kotlin exercise solved

---
+++ kotlin/exercise 1/src/Main.kt
@@ -0,0 +1,35 @@
+/*
+    MAKE A FUNCTION THAT VERIFIES IF A VALOR IS PERFECT OR NOT. A VALOR IS CALLED PERFECT WHEN
+    IT IS EQUAL THE SUM OF ITS DIVISORS EXCEPT ITSELF. EXAMPLE: 6 IS PERFECT BECAUSE 6 = 1 + 2 + 3
+    THE FUNCTION MUST RETURN A BOOLEAN RESULT
+* */
+
+
+fun main() {
+    val userNum = readln().toInt()
+
+    println(isPerfect(userNum))
+}
+
+fun isPerfect(num: Int): Boolean{
+    var res = 0
+    val divisors = ArrayList<Int>()
+    for(n in 1..num){
+        if(num % n == 0 && n < num){
+            divisors.add(n)
+        }
+    }
+
+    divisors.forEach {
+        println(it)
+        res += it
+    }
+
+    return if(res == num){
+        println("$num is perfect?")
+        true
+    }else{
+        println("$num is perfect?")
+        false
+    }
+}
