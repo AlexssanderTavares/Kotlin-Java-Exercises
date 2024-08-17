e2b668e8db5f0ae68b74a7a16eb73cb388f2f5f4
Alexssander Tavares <alexssander.tavares.dev@gmail.com>
2024-08-17 13:24:14 -0300

    Adding Java exercise 2

---
+++ Java/exercise 2/src/Main.java
@@ -0,0 +1,20 @@
+import java.sql.Array;
+
+//TIP CONSIDER AN ARRAY OF TRAJECTORIES OF 9 ELEMENTS, WHERE EACH ELEMENT HAS THE NEXT ARRAY ELEMENT ADDRESS TO BE READ.
+//    INDEX 1 2 3 4 5 6 7 8 9
+//    VALOR 5 7 6 9 2 8 4 0 3
+//
+//    SO THE SEQUENCE OF READING WILL BE 1, 5, 2, 7, 4, 9, 3, 6, 8, 0
+//    BUILD AN ALGORITHM THAT IS ABLE TO READ THIS ARRAY AND FOLLOW ITS OWN TRAJECTORY.
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {1,5,7,6,9,2,8,4,0,3};
+
+        int counter = arr[0];
+
+        while(counter < arr.length){
+            System.out.println(arr[counter]);
+            counter = arr[counter];
+        }
+    }
+}
